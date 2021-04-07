e78e0f635b86e366efa76cd6d0993122193e6ba7
zdyang888 <zedongyang99@gmail.com>
2021-04-07 17:13:07 -0500

    add id to line dict, add questions and anwsers list

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,36 @@
 # Building a Chatbot with Deep NLP
+# import lib
+import numpy as np
+import tensorflow as tf
+import re
+import time
+import os
+
+
+## Data preprocessing ##
+# Import dataset
+filePath = 'cornell_movie_dialogs_corpus'
+lines = open(os.path.join(filePath,'movie_lines.txt'),encoding='utf-8',errors='ignore').read().split('\n')
+conversations = open(os.path.join(filePath,'movie_conversations.txt'),encoding='utf-8',errors='ignore').read().split('\n')
+
+# Build dictionary to connet the output and input.
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line)==5:
+        id2line[_line[0]] = _line[4]
+
+# Create a list of conversation
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversations_ids.append(_conversation.split(','))
+
+# Getting separately the questions and the answers
+questions = []
+answers = []
+
+for conversation in conversations_ids:
+    for i in range(len(conversation)-1):
+        questions.append(conversation[i])
+        answers.append(conversations[i+1])
--- getdata.sh
+++ getdata.sh
@@ -3,3 +3,4 @@ unzip cornell_movie_dialogs_corpus.zip
 mv cornell\ movie-dialogs\ corpus/ cornell_movie_dialogs_corpus 
 rm cornell_movie_dialogs_corpus.zip
 rm -rf __MACOSX
+echo cornell_movie_dialogs_corpus >> .gitignore

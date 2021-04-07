aff0f2860233810557bf5ce3360dd52c4fbdb7e4
zdyang888 <zedongyang99@gmail.com>
2021-04-06 22:34:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a NLP chatbot project employing seq2seq model.
+Run 'bash getdata.sh' to acheive the data (Cornell Movie--Dialogs Corpus).
---
+++ getdata.sh
@@ -0,0 +1,6 @@
+wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
+unzip cornell_movie_dialogs_corpus.zip
+mv cornell\ movie-dialogs\ corpus/ cornell_movie_dialogs_corpus 
+rm cornell_movie_dialogs_corpus.zip
+rm -rf __MACOSX
+echo cornell_movie_dialogs_corpus >> .gitignore
